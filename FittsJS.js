ed2c135bde14f2686b8cdafe2218632634ca6de6
Camerontaylorr <46680421+Camerontaylorr@users.noreply.github.com>
2020-03-21 21:53:27 +0000

    Refactoring and changes

--- FittsJS.js
+++ FittsJS.js
@@ -1,80 +1,52 @@
-var count = 0;
-var lastYPost = 0;
-var lastXPost = 0;
-var newYPost = 0;
-var startTime = Date.now();
-window.addEventListener("DOMContentLoaded", function(){
-  let grid = document.querySelectorAll(".box");
-  getNumber()
-  Array.from(grid, function(box) {
-    box.addEventListener("mouseup", function() {
-      /*
-    console.log("X: " + (document.getElementById("Position").innerHTML = event.clientX ));
-    console.log("Y: " + (document.getElementById("Position").innerHTML = event.clientY));
-    console.log( "MISS")
-    */
-    count += 1;
-    getPerformance()  
-    }); 
-  });   
-});
+const box = document.querySelectorAll('.box')
+const target = document.querySelectorAll('.Target')
+
+let counter =0;
+var startID;
+var i = 0
 
 
-function getPerformance(){
-  endTime = Date.now();  
-  clickTiming = endTime - startTime;
-}
+function startbox(){
+	box.forEach(className => {
+		className.classList.remove('Target')
+		
+	})
+	startID = box[i]
+	startID.classList.add('Target');
+	hitPosition = startID.id;
+	}
 
-function getNumber()
-{
-  var counter = 0
-  var num = Math.floor(Math.random() * Math.floor(14));
-  var x = document.getElementById(num);
-  counter++;
-  x.style.background = "red";
+box.forEach(id => {
+	id.addEventListener('mouseup',() =>{
+			
+			if(id.id === hitPosition){
+			startID.classList.remove('Target')
+			let randomPos = box[Math.floor(Math.random()*14)]
+			randomPos.classList.add('Target')
 
-  previousID = x.id;
-  nextID = Math.floor(Math.random() * Math.floor(14));
+			startID = randomPos
+
+			hitPosition = randomPos.id					
+			console.log("Rand: " + randomPos.id)
+
+			randomPos = box[i]
+			randomPos.classList.add('Target')
+			console.log("Rand 2: " + randomPos.id)
 
 
-  lastClick = Date.now();  
-
-
-x.onclick = function()
- {
-  count + 1;
-  console.log("X: " + (document.getElementById("Position").innerHTML = event.clientX ));
-  console.log("Y: " + (document.getElementById("Position").innerHTML = event.clientY));
-  console.log("Cell Clicked: " + previousID)
-  console.log("next cell : " + nextID)
-  count + 1;
-  console.log(count);
-  currentClick = Date.now();  
-  clickTime = currentClick - lastClick;
-  console.log("Time: " + clickTime);
- 
-  x.style.background = "#6495ED";
-  x.onclick = function()
- {
-   console.log("MISS")
-   //count + 1;
-   console.log("Miss count" +count);
-}
-  getNumber()
-  console.log("HIT")
-  //console.log(Store)
-  counter--
- }
-
- if (count == 20 || count > 20) 
- {
-   
- // window.location.href = 'EndScreen.html';
-   alert("Task Complete. Time: " + clickTiming)
- }
-}
-
-function order()
-{
 
 }
+
+else 
+		   {
+			console.log("MISS")
+		   }
+		   
+	})
+
+
+
+})
+
+	
+	startbox();
